7c1a0fd384f54994327c1355d40c36d509ef83a2
VladimirNikiforov <developer.vn.d@gmail.com>
2019-02-03 15:34:49 +0300

    HomeWork3 final

--- sql/shell/hw3_keywords_export.sh
+++ sql/shell/hw3_keywords_export.sh
@@ -2,4 +2,4 @@
 #Filename: hw3_keywords_export.sh
 
 echo "Выгружаем таблицу в текстовый файл с разделителями - табуляцией..."
-psql -U postgres -c "\copy (select * from top_rated_tags) to '/raw_data/top_rated_tags.tsv' with delimiter as E'\t'"
+psql -U postgres -c "\copy (select * from top_rated_tags) to '/usr/local/share/netology/raw_data/top_rated_tags.tsv' with delimiter as E'\t'"
